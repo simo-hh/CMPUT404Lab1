208e1d02e2d98006dc2d34eb0fcd0c6c3dc965fc
abbott <51351558+abbotthh@users.noreply.github.com>
2022-01-10 18:24:13 -0700

    Update code

--- script.py
+++ script.py
@@ -1,4 +1,5 @@
 import requests as re
 
 #print(re.__version__)
-print(re.get(url = 'https://www.google.com').text)
+#print(re.get(url = 'https://www.google.com').text)
+print(re.get(url = 'https://raw.githubusercontent.com/simo-hh/CMPUT404Lab1/master/script.py').text)
